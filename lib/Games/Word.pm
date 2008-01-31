b1ae1086a4fcfa69c7fa70e5964923a7c9e1003c
jluehrs2 <jluehrs2@uiuc.edu>
2008-01-30 21:34:54 -0500

    Initial import of Games::Word

---
+++ lib/Games/Word.pm
@@ -0,0 +1,83 @@
+#!perl
+package Games::Word;
+use strict;
+use warnings;
+
+
+
+=head1 NAME
+
+Games::Word - ???
+
+=head1 VERSION
+
+Version 0.01 released ???
+
+=cut
+
+our $VERSION = '0.01';
+
+=head1 SYNOPSIS
+
+    use Games::Word;
+    do_stuff();
+
+=head1 DESCRIPTION
+
+
+
+=head1 SEE ALSO
+
+L<Foo::Bar>
+
+=head1 AUTHOR
+
+Jesse Luehrs, C<< <jluehrs2 at uiuc.edu> >>
+
+=head1 BUGS
+
+No known bugs.
+
+Please report any bugs through RT: email
+C<bug-games-word at rt.cpan.org>, or browse
+L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=Games-Word>.
+
+=head1 SUPPORT
+
+You can find this documentation for this module with the perldoc command.
+
+    perldoc Games::Word
+
+You can also look for information at:
+
+=over 4
+
+=item * AnnoCPAN: Annotated CPAN documentation
+
+L<http://annocpan.org/dist/Games-Word>
+
+=item * CPAN Ratings
+
+L<http://cpanratings.perl.org/d/Games-Word>
+
+=item * RT: CPAN's request tracker
+
+L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=Games-Word>
+
+=item * Search CPAN
+
+L<http://search.cpan.org/dist/Games-Word>
+
+=back
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright 2008 Jesse Luehrs.
+
+This program is free software; you can redistribute it and/or modify it
+under the same terms as Perl itself.
+
+=cut
+
+1;
+

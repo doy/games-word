d1d1bb8243b2a0ac87405c8d4ae326858310c60b
jluehrs2 <jluehrs2@uiuc.edu>
2008-12-17 23:54:50 -0500

    better usage

--- bin/solve-spook.pl
+++ bin/solve-spook.pl
@@ -3,7 +3,7 @@ use strict;
 use warnings;
 use Games::Word::Wordlist;
 
-die "Usage: $0 <subword>\n" unless @ARGV;
+die "Usage: $0 <letter_pool>\n" unless @ARGV;
 my $wl = Games::Word::Wordlist->new('/usr/share/dict/words');
 my $re = join '.*', sort split(//, $ARGV[0]);
 $wl->each_word(sub {
--- bin/solve-superghost.pl
+++ bin/solve-superghost.pl
@@ -3,6 +3,6 @@ use strict;
 use warnings;
 use Games::Word::Wordlist;
 
-die "Usage: $0 <word_prefix>\n" unless @ARGV;
+die "Usage: $0 <subword>\n" unless @ARGV;
 my $wl = Games::Word::Wordlist->new('/usr/share/dict/words');
 print "$_\n" for $wl->words_like(qr/\Q$ARGV[0]/i);

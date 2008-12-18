c4dd5a42a60a2c6cb6c96f617ade98a08ddaf77f
jluehrs2 <jluehrs2@uiuc.edu>
2008-12-17 23:54:11 -0500

    don't know why i was using $start here

--- bin/solve-superghost.pl
+++ bin/solve-superghost.pl
@@ -3,6 +3,6 @@ use strict;
 use warnings;
 use Games::Word::Wordlist;
 
-my $start = $ARGV[0] or die "Usage: $0 <word_prefix>\n";
+die "Usage: $0 <word_prefix>\n" unless @ARGV;
 my $wl = Games::Word::Wordlist->new('/usr/share/dict/words');
 print "$_\n" for $wl->words_like(qr/\Q$ARGV[0]/i);
--- bin/solve-xghost.pl
+++ bin/solve-xghost.pl
@@ -3,7 +3,7 @@ use strict;
 use warnings;
 use Games::Word::Wordlist;
 
-my $start = $ARGV[0] or die "Usage: $0 <subword>\n";
+die "Usage: $0 <subword>\n" unless @ARGV;
 my $wl = Games::Word::Wordlist->new('/usr/share/dict/words');
 my $re = join '.*', split(//, $ARGV[0]);
 print "$_\n" for $wl->words_like(qr/$re/i);
